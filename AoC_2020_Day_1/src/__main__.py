578dbb937209dfbeee0473672f836c6f831e3b59
devzh521 <devzh521@github.com>
2021-09-29 00:20:13 +0200

    refactor to accommodate for Day-1-2 solution

--- AoC_2020_Day_1/src/__main__.py
+++ AoC_2020_Day_1/src/__main__.py
@@ -6,10 +6,10 @@ https://adventofcode.com/2020/day/1
 
 print("Starting advent of code 2020 day 1")
 
-FILE = 'data/aoc_2020_day_1_1_data_task.txt'
+FILE = 'data/aoc_2020_day_1_data_task.txt'
 TOTAL = 2020
 
 with open(FILE) as f:
     numbers = [int(line.strip()) for line in f]
 
-print("Advent of code 2020 day 1 solved: %s" % D11.find_product(numbers, TOTAL))
+print("Advent of code 2020 day 1 solved: %s" % D11.get_two_entries_product(numbers, TOTAL))
