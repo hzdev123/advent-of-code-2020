3b7339d4efed97c66b40c58b01ea8d0f9505b81f
devzh521 <devzh521@github.com>
2021-09-28 23:51:28 +0200

    src files for solving day-3-1

---
+++ AoC_2020_Day_3/src/__main__.py
@@ -0,0 +1,12 @@
+from Day_3_1 import Day_3_1 as D31
+
+'''
+https://adventofcode.com/2020/day/3
+'''
+print("Starting advent of code 2020 day 3")
+
+FILE = 'data/aoc_2020_day_3_1_data_large.txt'
+with open(FILE) as f:
+    lines = [line.strip() for line in f]
+
+print("Advent of code 2020 day 3 solved: %s" % D31.get_nbr_trees_encountered(lines))
