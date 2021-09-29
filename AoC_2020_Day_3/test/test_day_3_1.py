03607c2ec61e24cfe4fcadda66ae50b913529aa8
devzh521 <devzh521@github.com>
2021-09-29 11:30:41 +0200

    refactor: function renaming

--- AoC_2020_Day_3/test/test_day_3_1.py
+++ AoC_2020_Day_3/test/test_day_3_1.py
@@ -1,10 +1,10 @@
 from src.Day_3_1 import Day_3_1 as D31
 
-def test_small_input():
+def test_example_input():
     file = 'data/aoc_2020_day_3_data_example.txt'
     assert get_nbr_trees_encountered(file) == 7
 
-def test_large_input():
+def test_task_input():
     file = 'data/aoc_2020_day_3_data_task.txt'
     assert get_nbr_trees_encountered(file) == 148
 
