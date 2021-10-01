a7d95cd33fd27fb0dc6c0edc4b1a2a1d436db1da
devzh521 <devzh521@github.com>
2021-10-01 02:25:46 +0200

    Data_Generator and Compare updated according to map solution

--- AoC_2020_Day_1/src/Compare.py
+++ AoC_2020_Day_1/src/Compare.py
@@ -6,16 +6,16 @@ Class used for comparing performance between brute force
 and position tracking solution.
 """
 
-n = 500
+n = 15
 
-title_d11 = "2D Brute Force vs Position Tracking"
-title_d12 = "3D Brute Force vs Position Tracking"
+title_d11 = "2D Brute Force vs Map solution"
+title_d12 = "3D Brute Force vs Map solution"
 
 x = list(range(1,n + 1))
-y_d11_brute = DG.get_Brute_Data(n, 2)
-y_d12_brute = DG.get_Brute_Data(n, 3)
-y_d11_ok = DG.get_Day_1_1_Tracked_Data(n)
-y_d12_ok = DG.get_Day_1_2_Tracked_Data(n)
+y_d11_brute = DG.get_Data_Points(n, 2)
+y_d12_brute = DG.get_Data_Points(n, 3)
+y_d11_map = DG.get_Data_Points(n, 1)
+y_d12_map = DG.get_Data_Points(n, 2)
 
-Graph.draw(x, y_d11_brute, y_d11_ok, title_d11)
-Graph.draw(x, y_d12_brute, y_d12_ok, title_d12)
+Graph.draw(x, y_d11_brute, y_d11_map, title_d11)
+Graph.draw(x, y_d12_brute, y_d12_map, title_d12)
--- AoC_2020_Day_1/src/Data_generator.py
+++ AoC_2020_Day_1/src/Data_generator.py
@@ -4,11 +4,9 @@ class Data_generator:
     """
 
     @staticmethod
-    def get_Brute_Data(n, exp):
+    def get_Data_Points(n, exp):
         """
         Returns a list over the number of iterations from 1 to n + 1
-
-        These data points represent solving day-1 with brute force
 
         Parameters
         ----------
@@ -26,55 +24,4 @@ class Data_generator:
         data = []
         for i in range(1, n + 1):
             data.append(i**exp)
-        return data
-
-    @staticmethod
-    def get_Day_1_1_Tracked_Data(n):
-        """
-        Returns a list over the number of iterations from 1 to n + 1
-
-        These data points represent solving day-1-1 while tracking list position
-
-        Parameters
-        ----------
-        n : int
-            number of list entries
-
-        Returns
-        -------
-        list
-            contains the number of iterations from 1 to n + 1
-
-        """
-        data = []
-        for i in range(1, n + 1):
-            data.append(sum(range(1, i)))
-        return data
-
-    @staticmethod
-    def get_Day_1_2_Tracked_Data(n):
-        """
-        Returns a list over the number of iterations from 1 to n + 1
-
-        These data points represent solving day-1-2 while tracking list position
-
-        Parameters
-        ----------
-        n : int
-            number of list entries
-
-        Returns
-        -------
-        list
-            contains the number of iterations from 1 to n + 1
-
-        """
-        data = []
-        prev_sum = 0
-        for i in range(1, n + 1):
-            if i > 2:
-                prev_sum = sum(range(1, i - 1)) + prev_sum
-                data.append(prev_sum)
-            else:
-                data.append(0)
         return data
