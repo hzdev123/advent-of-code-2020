8cc5a075248e48404d671294df48a2195eb3b93d
devzh521 <devzh521@github.com>
2021-10-06 17:43:27 +0200

    refactor test/ according to open closed principle

--- AoC_2020_Day_4/test/Passport_check.py
+++ AoC_2020_Day_4/test/Passport_check.py
@@ -1,49 +1,23 @@
-import re
+from abc import ABC, abstractmethod
 
-class Passport_check:
+class Passport_check(ABC):
+    """
+    Abstract class for handling passport validity checking
+    """
 
-    @staticmethod
-    def valid(mandatory_fields, line):
-        for mandatory_field in mandatory_fields:
-            if mandatory_field not in line:
-                return False
-        return True
+    @abstractmethod
+    def check_passport(mandatory_fields, line):
+        """
+        Checks the validity of a passport
 
-    @staticmethod
-    def is_valid_fields(mandatory_fields, line):
-        BYR = ' byr:'
-        IYR = ' iyr:'
-        EYR = ' eyr:'
-        HGT = ' hgt:'
-        HCL = ' hcl:'
-        ECL = ' ecl:'
-        PID = ' pid:'
+        Parameters
+        ----------
+        mandatory_fields : list
+            list over required fields in passport
+        line : str
+            string representing the passport
 
-        if line is None or \
-            BYR not in line or \
-            IYR not in line or \
-            EYR not in line or \
-            HGT not in line or \
-            HCL not in line or \
-            ECL not in line or \
-            PID not in line:
-            return False
-
-        for f in mandatory_fields:
-            field_regex = r".*%s(\S+)\s?.*" % f
-            f_val = re.sub(field_regex, r"\1", line)
-
-            if (f == BYR and (not 1920 <= int(f_val) <= 2002)) or \
-                (f == IYR and (not 2010 <= int(f_val) <= 2020)) or \
-                (f == EYR and (not 2020 <= int(f_val) <= 2030)) or \
-                (f == HCL and (re.search(r"^#[0-9a-f]{6}$", f_val)) is None) or \
-                (f == PID and (re.search(r"^\d{9}$", f_val) is None)) or \
-                (f == HGT and ('cm' not in f_val and 'in' not in f_val)) or \
-                (f == HGT and \
-                    ('cm' in f_val) and (not 150 <= int(f_val.replace("cm", "")) <= 193) or \
-                (f == HGT and \
-                    ('in' in f_val) and (not 59 <= int(f_val.replace("in", "")) <= 76)) or \
-                (f == ECL and \
-                    (re.search("^(amb|blu|brn|gry|grn|hzl|oth)$", f_val) is None))):
-                return False
-        return True
+        Returns
+        -------
+        """
+        pass
---
+++ AoC_2020_Day_4/test/test_day_4_task.py
@@ -0,0 +1,16 @@
+from Day_4 import Day_4 as D4
+from Day_4_1_check import Day_4_1_check as d1
+from Day_4_2_check import Day_4_2_check as d2
+from Passport_check import Passport_check as PC
+
+def test_day_4_example_config():
+    ans = [
+        2,
+        2
+    ]
+    FILE = 'data/aoc_2020_day_4_data_example.txt'
+    with open(FILE) as f:
+        lines = [line.strip() for line in f]
+
+    for idx, pass_check in enumerate(PC.__subclasses__()):
+        assert D4.get_nbr_of_valid_passports(pass_check.check_passport, lines) == ans[idx]
